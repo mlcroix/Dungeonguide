fa0d11e7a60108d3b8259c28bb3fdd2bc0794074
Michel du Croix <mlcroix@avans.nl>
2018-12-30 17:07:14 +0100

    sign-up and snackbar

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,10 +2,9 @@ import { NgModule } from '@angular/core';
 import { AuthService } from './auth.service';
 import { LoginComponent } from './login.component';
 import { SignUpComponent } from './signUp.component';
-import { FormsModule } from '@angular/forms';
 import { AppMaterialModule } from '../app/app.material.module';
 import { LocalStorageService } from '../app/app.localStorageService';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -15,10 +14,13 @@ import { LocalStorageService } from '../app/app.localStorageService';
     imports: [
         FormsModule,
         AppMaterialModule,
+        FormsModule,
+        ReactiveFormsModule,
     ],
     exports: [
         LoginComponent,
-        SignUpComponent
+        SignUpComponent,
+        ReactiveFormsModule,
     ],
     providers: [
         AuthService,
--- src/auth/signUp.component.ts
+++ src/auth/signUp.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { LocalStorageService } from '../app/app.localStorageService';
+import { FormGroup, FormControl } from '@angular/forms';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
     selector: 'app-signup-selector',
@@ -12,23 +14,58 @@ import { LocalStorageService } from '../app/app.localStorageService';
 export class SignUpComponent {
   localStorage: LocalStorageService;
   authService: AuthService;
-    title = 'Sign up';
-    loggedIn = false;
+  title = 'Sign up';
+  signupForm: FormGroup;
+
+  constructor(private Service: AuthService, public snackBar: MatSnackBar) {
+    this.localStorage = new LocalStorageService();
+    this.authService = Service;
+
+    this.signupForm = new FormGroup({
+      firstname: new FormControl(),
+      lastname: new FormControl(),
+      email: new FormControl(),
+      username: new FormControl(),
+      password: new FormControl(),
+      password2: new FormControl()
+    });
+  }
 
 
+  public signUp(signup: NgForm) {
+    this.authService.SignUp(signup).then((result) => {
+      const message = result.json().message;
 
-    constructor(private Service: AuthService) {
-      this.localStorage = new LocalStorageService();
-      this.authService = Service;
-    }
-
-
-    public login(signup: NgForm) {
-
-      console.log(signup);
-      this.authService.SignUp(signup).then((result) => {
+      if (result.status === 200) {
         this.localStorage.setItem('user', JSON.stringify(result));
         window.location.reload();
-      });
+      } else {
+        this.snackBar.open(message, '', {
+          duration: 5000,
+          panelClass: ['snack-bar']
+        });
+      }
+    });
+  }
+
+  public isValid() {
+    let valid = true;
+
+    if (!this.checkPasswords()) {
+      valid = false;
+    }
+
+    return valid;
+  }
+
+  public checkPasswords() {
+    const pass1 = this.signupForm.get('password').value;
+    const pass2 = this.signupForm.get('password2').value;
+
+    if (pass1 === pass2) {
+      return true;
+    }
+
+    return false;
   }
 }
